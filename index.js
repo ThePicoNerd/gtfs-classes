9ea3aa9efb47feb288aa8253b599dda4a6e10e8c
Åke Amcoff <ake.amcoff@lynx.agency>
2019-04-21 18:28:27 +0200

    stop_sequence is now always parsed as a number

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ class StopTime {
     this.arrival_time = arrival_time;
     this.departure_time = departure_time;
     this.stop_id = stop_id;
-    this.stop_sequence = stop_sequence;
+    this.stop_sequence = parseInt(stop_sequence);
     this.stop_headsign = stop_headsign;
     this.pickup_type = pickup_type;
     this.drop_off_type = drop_off_type;
